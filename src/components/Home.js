c80db7435605950d85d60635096502341ee25f32
Martin <joewwi03@gmail.com>
2023-04-20 08:41:58 +0300

    Completed Asignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">
+    <h1 style={{color:"firebrick"}}>Your Name is a Web Developer from Your City</h1>
+    <h2>{name}</h2>
+    <h4>{city}</h4>
+  </div>
+  )
 }
 
 export default Home;
+ 
